daaea760d8e43c294ea8d9eb40712641e0833cb9
hakim-chaabouchi <hakimchaabouchi@gmail.com>
2024-04-20 17:13:30 +0100

    second_commit

---
+++ src/components/Second.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>This is h2 of second vue</h2>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    msg: String
+})
+
+</script>
+
+<style scoped>
+h2 {
+    color: green;
+}
+
+</style>
---
+++ src/components/first.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+           <div class="c1">Hellow World  {{ p }} et {{ d [2]}}</div>
+           <SecondVue msg="this is the msg of"/>
+           <button @click="func_1">btn</button>
+    </div>
+</template>
+
+<script setup>
+    import SecondVue from './Second.vue'
+
+
+   const p = 'paragraphe'
+   const d = [1, 2,3]
+   const func_1 = () => {
+    console.log(' this is func_1 ()');
+   }
+   
+ 
+
+</script>
+
+<style scoped>
+.c1 {
+    color: brown;
+    background-color: aquamarine;
+   
+}
+
+</style>
